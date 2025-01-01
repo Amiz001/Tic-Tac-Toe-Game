aaa82b01c4a3afa1d63da2ff504cb1e92b1568a3
Amiz <lgmalshan@gmail.com>
2025-01-02 00:31:56 +0530

    setup initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function checkResult(arr) {
+  const wins = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 4, 8], 
+    [2, 4, 6], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+  ];
+
+  const array = Array.from(arr).map(a => a.textContent);
+
+  for (let win of wins) {
+    const [a, b, c] = win;
+
+    if (array[a] === "X" && array[b] === "X" && array[c] === "X") {
+      window.alert("Winner is Player X");
+      return true;
+    } else if (array[a] === "O" && array[b] === "O" && array[c] === "O") {
+      window.alert("Winner is Player O");
+      return false;
+    }
+  }
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  let symbol = true;
+  const boxes = document.querySelectorAll(".btn");
+  boxes.forEach(box => {
+    box.addEventListener("click", () => {
+
+      if(symbol && box.textContent == ""){
+        box.textContent = "X"
+        symbol = false;
+      }
+      else if(!symbol && box.textContent == ""){
+        box.textContent = "O"
+        symbol = true;
+      }
+    })
+  })
 
   return (
-    <>
+    <>  
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <button className="btn"></button>
+      <button className="btn"></button>
+      <button className="btn"></button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+      <button className="btn"></button>
+      <button className="btn"></button>
+      <button className="btn"></button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+      <button className="btn"></button>
+      <button className="btn"></button>
+      <button className="btn"></button>
+      </div>
     </>
   )
 }
